1714878d059943fc5ea0b3117404afd926b1a219
mosalut <ludaoyuan@mosalut.com>
2021-09-08 13:51:24 +0800

    Finish framework and ajax message
    Changes to be committed:
    	modified:   css/index.css
    	deleted:    css/login.css
    	new file:   css/signin.css
    	new file:   css/signinsuccess.css
    	modified:   index.html
    	modified:   js/global.js
    	modified:   js/index.js
    	new file:   js/signinsuccess.js
    	renamed:    login.html -> signin.html
    	new file:   signinsuccess.html

---
+++ js/index.js
@@ -0,0 +1,74 @@
+"use strict";
+
+var getCapitalBXmlhttp;
+var getLowcaseBXmlhttp;
+
+function getCData(e) {
+	if (e.target.readyState == 4 && e.target.status == 200) {
+		console.log(e.target.responseText);
+		let response = JSON.parse(e.target.responseText);
+		console.log(response, "C");
+		let locked = document.getElementById("lockedB");
+		let unlocked = document.getElementById("unlockedB");
+
+		if(!response.success){
+			if(response.message == "not auth") {
+				sessionStorage.clear();
+				window.location.href = "/signin.html";
+				return;
+			}
+			locked.innerText = "unknown";
+			unlocked.innerText = "unknown";
+			return;
+		}
+
+		locked.innerText = response.data.locked;
+		unlocked.innerText = response.data.unlocked;
+	}
+}
+
+function getLData(e) {
+	if (e.target.readyState == 4 && e.target.status == 200) {
+		console.log(e.target.responseText);
+		let response = JSON.parse(e.target.responseText);
+		console.log(response, "L");
+		let b = document.getElementById("b");
+
+		if(!response.success){
+			b.innerText = "unknown";
+			return;
+		}
+
+		b.innerText = response.data;
+	}
+}
+
+var key = sessionStorage.getItem("Bb_key");
+var account = sessionStorage.getItem("Bb_account");
+function main() {
+	console.log(key);
+	console.log(account);
+	if(key == null || account == null) {
+		window.location.href = "/signin.html";
+		return;
+	}
+
+	if (window.XMLHttpRequest) {
+		getCapitalBXmlhttp = new XMLHttpRequest();
+		getLowcaseBXmlhttp = new XMLHttpRequest();
+	} else {
+		getCapitalBXmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		getLowcaseBXmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+
+	getCapitalBXmlhttp.open("GET", "http://47.98.204.151:8888/getcapitalb?account=" + account + "&key=" + key);
+	getLowcaseBXmlhttp.open("GET", "http://47.98.204.151:8888/getlowcaseb?account=" + account + "&key=" + key);
+
+	getCapitalBXmlhttp.send();
+	getLowcaseBXmlhttp.send();
+
+	getCapitalBXmlhttp.onreadystatechange = getCData;
+	getLowcaseBXmlhttp.onreadystatechange = getLData;
+}
+
+window.onload = main;
---
+++ js/signinsuccess.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function GetQueryString(name) {
+     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+     var r = window.location.search.substr(1).match(reg);
+     if(r!=null)return  unescape(r[2]); return null;
+}
+
+function main() {
+	let key = GetQueryString("key");
+	let account = GetQueryString("account");
+	if(key == null || account == null) {
+		window.location.href = "/signin.html";
+		return;
+	}
+
+	sessionStorage.setItem("Bb_key", key);
+	sessionStorage.setItem("Bb_account", account);
+
+	let success = document.getElementsByTagName("input")[0];
+	success.value = "sign in success";
+}
+
+window.onload = main;
